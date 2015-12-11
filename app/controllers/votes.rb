b60a2ececaaf9b9e42957bcdc2c6025c5790ec84
Danielle Cameron & Steven Broderick <pair+danyell85.stevula@devbootcamp.com>
2015-12-11 14:00:52 -0800

    Make so user can only vote once on each post. Add image to readme

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -3,11 +3,17 @@ post '/posts/:id/votes' do
   value = params[:value] == 'up' ? 1 : -1
 
   if current_user
-    Vote.create(
+    new_vote = Vote.new(
       voter_id: current_user.id,
       post_id: params[:id],
       value: value
       )
+
+    unless new_vote.save
+      old_vote = Vote.find_by(voter_id: current_user.id, post_id: params[:id])
+      old_vote.destroy
+      new_vote.save
+    end
   end
 
   if post.post_type == "question"
