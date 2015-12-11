54896e4dbf98544006d540d5c4a292d8d060f2b7
Danielle Cameron & Steven Broderick <pair+danyell85.stevula@devbootcamp.com>
2015-12-11 12:33:05 -0800

    Add postinnng authorizations

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,16 +1,17 @@
 post '/posts/:id/comments' do
-  p params, current_user.id
+  if current_user
+    if params[:answer_id]
+      post_id = params[:answer_id]
+    else
+      post_id = params[:id]
+    end
 
-  if params[:answer_id]
-    post_id = params[:answer_id]
-  else
-    post_id = params[:id]
+    Comment.create(
+      body:params[:body],
+      post_id: post_id,
+      author_id:current_user.id
+      )
   end
 
-  Comment.create(
-    body:params[:body],
-    post_id: post_id,
-    author_id:current_user.id
-    )
   redirect "posts/#{params[:id]}"
 end
